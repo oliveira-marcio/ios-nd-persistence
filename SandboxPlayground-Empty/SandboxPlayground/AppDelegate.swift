72a3e4715b54a1bce80c6cf40ba7fd7aa8bf0626
oliveira-marcio <emaildomso@gmail.com>
2020-09-07 08:50:04 +0100

    feat: Write and read a plain txt file

--- SandboxPlayground-Empty/SandboxPlayground/AppDelegate.swift
+++ SandboxPlayground-Empty/SandboxPlayground/AppDelegate.swift
@@ -16,11 +16,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: Properties
     
     var window: UIWindow?
+    
+    func sandboxPlayground() {
+        let fileManager = FileManager.default
+        let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        let url = urls.last?.appendingPathComponent("file.txt")
+        
+        do {
+            try "Hi there!".write(to: url!, atomically: true, encoding: .utf8)
+        } catch {
+            print("Error while writing")
+        }
+        
+        do {
+            let content = try String(contentsOf: url!, encoding: .utf8)
+            
+            if content == "Hi there!" {
+                print("yay")
+            } else {
+                print("oops")
+            }
+        } catch {
+            print("Somthing went wrong")
+        }
+    }
 
     // MARK: UIApplicationDelegate
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        sandboxPlayground()
         return true
     }
 }

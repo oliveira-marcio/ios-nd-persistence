bfed3df691446dd3e94a5fa175c125e2a4450a52
oliveira-marcio <emaildomso@gmail.com>
2020-09-08 08:10:42 +0100

    feat: Add data controller

---
+++ Mooskine/Mooskine-starter/Mooskine/Model/DataController.swift
@@ -0,0 +1,31 @@
+//
+//  DataController.swift
+//  Mooskine
+//
+//  Created by Márcio Oliveira on 9/8/20.
+//  Copyright © 2020 Udacity. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+    let persistentContainer: NSPersistentContainer
+    
+    var viewContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    init(modelName: String) {
+        persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func load(completion: (() -> Void)? = nil) {
+        persistentContainer.loadPersistentStores { (storeDescription, error) in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+            completion?()
+        }
+    }
+}

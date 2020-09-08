a78931b663baac0fd2405117c3ea4dddba5515b3
oliveira-marcio <emaildomso@gmail.com>
2020-09-08 09:09:35 +0100

    refactor: Add and delete notebooks

--- Mooskine/Mooskine-starter/Mooskine/View Controllers/NotebooksListViewController.swift
+++ Mooskine/Mooskine-starter/Mooskine/View Controllers/NotebooksListViewController.swift
@@ -86,15 +86,20 @@ class NotebooksListViewController: UIViewController, UITableViewDataSource {
 
     /// Adds a new notebook to the end of the `notebooks` array
     func addNotebook(name: String) {
-//        TODO: add notebook
-//        let notebook = Notebook(name: name)
-//        notebooks.append(notebook)
-        tableView.insertRows(at: [IndexPath(row: numberOfNotebooks - 1, section: 0)], with: .fade)
+        let notebook = Notebook(context: dataController.viewContext)
+        notebook.name = name
+        notebook.creationDate = Date()
+        try? dataController.viewContext.save()
+        notebooks.insert(notebook, at: 0)
+        tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .fade)
         updateEditButtonState()
     }
 
     /// Deletes the notebook at the specified index path
     func deleteNotebook(at indexPath: IndexPath) {
+        let notebookToDelete = notebook(at: indexPath)
+        dataController.viewContext.delete(notebookToDelete)
+        try? dataController.viewContext.save()
         notebooks.remove(at: indexPath.row)
         tableView.deleteRows(at: [indexPath], with: .fade)
         if numberOfNotebooks == 0 {
